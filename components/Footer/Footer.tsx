06f0ce550f6d494388580fbc653ee681647de867
gangbeomsu <kbs21c1987@gmail.com>
2024-02-15 20:59:48 +0900

    푸터 완료

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer className="p-4 mt-auto border-t">Footer</footer>;
+  return (
+    <footer className="p-4 mt-auto border-t">
+      <div className="text-xs text-center">
+        Copyright &copy; [Dev Kang]. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
