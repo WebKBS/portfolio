2ea47406009dab8950cb86c9d89659b78d264af5
gangbeomsu <kbs21c1987@gmail.com>
2024-02-18 18:12:23 +0900

    홈 works 1차 완료

--- components/Works/Works.tsx
+++ components/Works/Works.tsx
@@ -1,5 +1,43 @@
-const Works = () => {
-  return <li>Works</li>;
+import { IWorks } from "@/works/worksData";
+import Image from "next/image";
+import Link from "next/link";
+import { Badge } from "../ui/badge";
+
+const Works = ({ filterData }: { filterData: IWorks[] }) => {
+  return (
+    <>
+      {filterData.map((work: IWorks) => (
+        <li
+          key={work.id}
+          className="max-w-60 flex-1 rounded-xl border bg-white px-3 py-4 shadow-md transition-all duration-300 ease-in-out hover:shadow-lg dark:border-gray-700 dark:bg-gray-800"
+        >
+          <Link href="">
+            <div className="relative mb-2 h-40 w-full overflow-hidden ">
+              <Image
+                src={work.image}
+                alt={work.thumbnail}
+                width={300}
+                height={300}
+                className="absolute left-0 top-0 h-auto w-full object-cover"
+              />
+            </div>
+            <div className="mx-2 mt-4 flex flex-col">
+              <div className="flex flex-col gap-2">
+                <h3 className="line-clamp-1 font-semibold">{work.title}</h3>
+                <p className="mb-4 line-clamp-2 text-sm">{work.description}</p>
+              </div>
+              <div className="flex justify-between">
+                <time className="text-sm" dateTime={work.date}>
+                  {work.date}
+                </time>
+                <Badge variant={"default"}>Detail</Badge>
+              </div>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </>
+  );
 };
 
 export default Works;
--- components/Works/WorksList.tsx
+++ components/Works/WorksList.tsx
@@ -1,30 +1,20 @@
-import { IWorks, works } from "@/works/worksData";
-import Image from "next/image";
+import { works } from "@/works/worksData";
+
 import Link from "next/link";
+import Works from "./Works";
 
 const WorksList = () => {
+  const filterData = works.slice(0, 4);
+  console.log(filterData);
+
   return (
     <>
       <h2 className="mb-12 text-center text-3xl font-bold">Works</h2>
-      <Link href="/works" className="inline-flex ">
+      <Link href="/works" className="">
         More +
       </Link>
-      <ul className="flex">
-        {works.map((work: IWorks) => (
-          <li key={work.id} className="max-w-60 flex-1 rounded-lg border">
-            <div className="relative h-40 w-full overflow-hidden">
-              <Image
-                src={work.image}
-                alt={work.thumbnail}
-                width={300}
-                height={300}
-                className="absolute left-0 top-0 h-auto w-full object-cover"
-              />
-            </div>
-            <h3>{work.title}</h3>
-            <p>{work.description}</p>
-          </li>
-        ))}
+      <ul className="flex justify-center gap-4">
+        <Works filterData={filterData} />
       </ul>
     </>
   );
