0d8270178d9093aaff4ec67f038fb78f77646e2b
gangbeomsu <kbs21c1987@gmail.com>
2024-02-22 00:58:16 +0900

    파일이름 에러 변경사항

---
+++ components/Lottie/Lottie.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
+import { Skeleton } from "../ui/skeleton";
+
+const LottieComp = () => {
+  const [animationData, setAnimationData] = useState<any>();
+
+  const styles = "mx-auto w-64 sm:mx-0 sm:ml-auto sm:mr-0 md:w-96";
+
+  useEffect(() => {
+    import("../../lottie/animation.json").then((res) => {
+      setAnimationData(res.default);
+    });
+  }, []);
+
+  if (!animationData)
+    return <Skeleton className={`${styles} h-64 rounded-full md:h-96`} />;
+
+  return (
+    <>
+      <Lottie className={styles} animationData={animationData} loop={true} />
+    </>
+  );
+};
+
+export default LottieComp;
