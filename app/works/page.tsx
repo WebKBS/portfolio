5e284f9063ee42b2acffee2ed4c0dcae467cd80e
gangbeomsu <kbs21c1987@gmail.com>
2024-02-27 00:29:16 +0900

    works 작업중

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+import { works } from "@/data/worksData";
+import { useRouterModalToggle } from "@/store/modal-store";
+import Link from "next/link";
+
+// export const metadata = {
+//   title: "Works",
+// };
+
 const WroksPage = () => {
-  return <div>WroksPage</div>;
+  const { setRouterModalState } = useRouterModalToggle();
+
+  const clickHandler = () => {
+    setRouterModalState(true);
+  };
+
+  // 클리언트 컴포넌트 분리하고, HOME에서 사용하는 works를 가져와서 사용
+
+  return (
+    <div>
+      <ul>
+        {works.map((work, index) => (
+          <li key={index}>
+            <Link
+              href={`/works/${work.slug}`}
+              onClick={clickHandler}
+              scroll={false}
+            >
+              {work.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default WroksPage;
