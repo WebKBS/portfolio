127cdc0feb53f03c162f5525074f211193bd1a0b
gangbeomsu <kbs21c1987@gmail.com>
2024-03-01 02:54:53 +0900

    배너 수정완료

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -11,9 +11,9 @@ const WroksPage = () => {
   return (
     <main className="flex-1">
       <WorksBanner image="/banner/works-banner.png" title="WORKS" />
-      <section className="relative z-[1] mx-auto max-w-screen-lg px-5 pb-20 pt-8 lg:max-w-screen-2xl lg:px-4 xl:px-6">
+      <section className="relative z-[1] mx-auto max-w-screen-lg  pb-20 pt-8 lg:max-w-screen-2xl lg:px-4 xl:px-6">
         <PageTitle title="WORKS" />
-        <ul className="flex flex-wrap gap-4 sm:grid sm:grid-cols-2 sm:grid-rows-[repeat(auto-fill,minmax(0,1fr))] lg:grid-cols-3 xl:grid-cols-4">
+        <ul className="flex flex-wrap gap-4 px-5 sm:grid sm:grid-cols-2 sm:grid-rows-[repeat(auto-fill,minmax(0,1fr))] lg:grid-cols-3 xl:grid-cols-4 xl:px-6">
           <Works filterData={works} />
         </ul>
       </section>
