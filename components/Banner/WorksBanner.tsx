774759712e1556326dee02d3646bc32f5ab12c08
gangbeomsu <kbs21c1987@gmail.com>
2024-03-01 02:47:01 +0900

    ios zindex test

--- components/Banner/WorksBanner.tsx
+++ components/Banner/WorksBanner.tsx
@@ -45,7 +45,7 @@ const WorksBanner = ({
   return (
     <div
       ref={bannerRef}
-      className="sticky left-0 top-14 z-0 h-[400px] w-screen overflow-hidden transition-opacity duration-300"
+      className="sticky left-0 top-14 z-[-1] h-[400px] w-screen overflow-hidden transition-opacity duration-300"
     >
       <Image
         priority
