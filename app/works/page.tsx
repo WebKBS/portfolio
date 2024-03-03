d4cc596bd2316795e8d5f0c265b84bd9195e61d2
gangbeomsu <kbs21c1987@gmail.com>
2024-03-03 20:46:53 +0900

    page 에러 수정

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,6 +1,6 @@
-import Works from "@/app/_components/Sections/Works/WorkItem";
 import WorksBanner from "@/components/Banner/WorksBanner";
 import { works } from "@/data/worksData";
+import WorkItem from "../_components/Sections/Works/WorkItem";
 import PageTitle from "./_components/PageTitle";
 
 export const metadata = {
@@ -14,7 +14,9 @@ const WroksPage = () => {
       <section className="relative z-[1] mx-auto max-w-screen-lg  pb-20 pt-8 lg:max-w-screen-2xl lg:px-6">
         <PageTitle title="WORKS" />
         <ul className="flex flex-wrap gap-4 px-5 sm:grid sm:grid-cols-2 sm:grid-rows-[repeat(auto-fill,minmax(0,1fr))] lg:grid-cols-3 lg:px-0 xl:grid-cols-4">
-          <Works filterData={works} />
+          {works.map((work) => (
+            <WorkItem key={work.id} work={work} />
+          ))}
         </ul>
       </section>
     </main>
