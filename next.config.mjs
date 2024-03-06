68fd8ce59d7f5520882b869037f9044941de02bd
gangbeomsu <kbs21c1987@gmail.com>
2024-03-06 22:16:44 +0900

    도커 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: 'openweathermap.org' }],
+    remotePatterns: [{ hostname: "openweathermap.org" }],
   },
 };
 
