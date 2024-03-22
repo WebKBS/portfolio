b598e1886791a998b233d868607d5d7fcecbc439
gangbeomsu <kbs21c1987@gmail.com>
2024-03-22 11:12:44 +0900

    블로그 아이콘 및 데이터 추가

--- components/Sns/Sns-data.ts
+++ components/Sns/Sns-data.ts
@@ -1,14 +1,20 @@
 export const sns = [
   {
-    title: 'Github',
-    link: 'https://github.com/WebKBS',
-    icon: '/icons/github.svg',
-    iconBlack: '/icons/github-black.svg',
+    title: "Github",
+    link: "https://github.com/WebKBS",
+    icon: "/icons/github.svg",
+    iconBlack: "/icons/github-black.svg",
   },
   {
-    title: 'Gitbook',
-    link: 'https://dev-k.gitbook.io/main/',
-    icon: '/icons/gitbook.svg',
-    iconBlack: '/icons/gitbook-black.svg',
+    title: "Blog",
+    link: "https://blog-webkbs.vercel.app/",
+    icon: "/icons/blog.svg",
+    iconBlack: "/icons/blog-black.svg",
+  },
+  {
+    title: "Gitbook",
+    link: "https://dev-k.gitbook.io/main/",
+    icon: "/icons/gitbook.svg",
+    iconBlack: "/icons/gitbook-black.svg",
   },
 ];
