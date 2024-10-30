a9b806056cf33ba7ac661df79c66a0aab8e482a7
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-30 20:53:04 +0900

    modify: Best section 디자인 변경

--- app/_components/Sections/BestWorks/BestSection.tsx
+++ app/_components/Sections/BestWorks/BestSection.tsx
@@ -30,69 +30,46 @@ const bestWorks = [
     description:
       "Project 4 description Project 4 description Project 4 description Project 4 description Project 4 description Project 4 description",
   },
-  {
-    id: 5,
-    title: "Project 5",
-    img: namhaeImages.desktopImage,
-    description:
-      "Project 5 description Project 5 description Project 5 description Project 5 description Project 5 description Project 5 description",
-  },
-  {
-    id: 6,
-    title: "Project 6",
-    img: namhaeImages.desktopImage,
-    description:
-      "Project 6 description Project 6 description Project 6 description Project 6 description Project 6 description Project 6 description",
-  },
 ];
 
 const BestSection = () => {
   const positionIndex = (index: number) => {
-    switch (index) {
-      case 0:
-        return "top-[120px]";
-      case 1:
-        return "top-[150px]";
-      case 2:
-        return "top-[180px]";
-      case 3:
-        return "top-[210px]";
-      case 4:
-        return "top-[240px]";
-      case 5:
-        return "top-[270px]";
-      default:
-        return "";
-    }
+    const positions = [
+      "top-[100px]",
+      "top-[140px]",
+      "top-[180px]",
+      "top-[220px]",
+    ];
+    return positions[index] || "";
   };
 
   return (
     <section className="mx-auto w-full max-w-[1920px] px-4 md:px-6">
-      <h2 className="mb-6 py-20 text-center text-3xl font-bold uppercase">
+      <h2 className="mb-6 py-20 text-center text-4xl font-extrabold uppercase tracking-wider">
         BEST WORKS
       </h2>
-      <div>
-        <ul className="flex flex-col gap-[160px]">
+      <div className="px-8">
+        <ul className="flex flex-col gap-[120px]">
           {bestWorks.map((work, index) => (
             <li
               key={work.id}
               className={cn(
-                "sticky left-0 bg-background",
+                "sticky left-0 rounded-lg bg-background shadow-lg transition-all duration-500 hover:scale-105",
                 positionIndex(index),
               )}
             >
-              <div className="relative flex min-h-screen overflow-hidden">
-                <div className="w-full md:w-[50%]">
-                  <h3 className="text-2xl font-bold">{work.title}</h3>
-                  <p className="text-sm">{work.description}</p>
+              <div className="relative flex min-h-[80vh] overflow-hidden rounded-lg bg-white shadow-md">
+                <div className="relative z-[2] flex w-full flex-col space-y-4 p-8 md:w-[50%]">
+                  <h3 className="text-3xl font-semibold">{work.title}</h3>
+                  <p className="">{work.description}</p>
                 </div>
-                <div className="absolute right-0 top-0 z-[-1] h-[calc(100%-200px)] w-full -skew-x-6 overflow-hidden">
+                <div className="absolute right-0 top-0 h-full w-full overflow-hidden bg-gray-800/50 transition duration-300 hover:bg-gray-800/30">
                   <Image
                     src={work.img}
                     alt={work.title}
-                    className="absolute right-12 top-0 max-w-3xl object-cover"
+                    className="absolute right-12 top-0 max-w-3xl -skew-x-6 object-cover opacity-90 transition-all duration-300 hover:scale-110 hover:opacity-100"
                   />
-                  <p className="absolute bottom-0 right-0 h-full w-full bg-background/70" />
+                  <p className="absolute bottom-0 right-0 h-full w-full bg-gray-800/50" />
                 </div>
               </div>
             </li>
