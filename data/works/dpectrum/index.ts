ea4cf69a767531646d7bcd472010bc8b89ea5a1b
kangbeomsu <kbs21c1987@gmail.com>
2025-10-18 20:06:33 +0900

    add: works 데이터 추가

--- data/works/dpectrum/index.ts
+++ data/works/dpectrum/index.ts
@@ -6,6 +6,45 @@ export const dpectrumWorks: IWorks[] = [
   {
     published: false,
     id: "dpectrum" + index++,
+    title: "GOLDIE 금매입 서비스 플랫폼",
+    description: "금매입 플랫폼 서비스 웹앱 개발",
+    thumbnail: "",
+    image: "",
+    mobileImage: null,
+    bannerImage: "",
+    url: null,
+    date: "2025.04 ~ 2025.10",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "Zustand",
+      "Tanstack Query",
+      "Tailwind CSS",
+    ],
+    slug: "goldie",
+    body: "금매입 플랫폼 서비스 웹앱 개발 프로젝트로, 사용자들이 금을 매입하고 판매할 수 있는 기능을 제공합니다.",
+    contribution: "백엔드(1명) 디자이너(1명) 프론트엔드(1명)",
+    detailContent: {
+      previewImages: [
+        {
+          desktopImage: "",
+          mobileImage: "",
+        },
+      ],
+      description: "",
+      insight: "",
+      stacks: ["", "", ""],
+      functions: [
+        {
+          function: "",
+          description: "",
+        },
+      ],
+    },
+  },
+  {
+    published: false,
+    id: "dpectrum" + index++,
     title: "BREAK 트레이딩 카드 거래 플랫폼",
     description: "트레이딩 카드 거래 플랫폼 웹앱 개발",
     thumbnail: "",
@@ -37,7 +76,7 @@ export const dpectrumWorks: IWorks[] = [
     },
   },
   {
-    published: true,
+    published: false,
     id: "dpectrum" + index++,
     title: "UHC 백오피스",
     description: "UHC 글로벌 호텔 서비스 백오피스 개발",
