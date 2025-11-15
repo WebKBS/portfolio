c6d5576ed39321e43c1bff6f9b5fb017c1a99fe4
kangbeomsu <kbs21c1987@gmail.com>
2025-11-15 17:07:53 +0900

    fix: 프로젝트 상세 페이지 타이틀 제거 및 공통 버튼 cursor 추가

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,31 +1,31 @@
-import WorksBanner from "@/components/Banner/WorksBanner";
-import { works } from "@/data/works/worksData";
-import WorkItem from "@/containers/home/Sections/Works/WorkItem";
-import PageTitle from "../../containers/works/PageTitle";
-import worksBannerImage from "@/public/images/banner/works-banner.png";
-import { IWorks } from "@/types/worksType";
+import WorksBanner from '@/components/Banner/WorksBanner';
+import { works } from '@/data/works/worksData';
+import WorkItem from '@/containers/home/Sections/Works/WorkItem';
+import PageTitle from '../../containers/works/PageTitle';
+import worksBannerImage from '@/public/images/banner/works-banner.png';
+import { IWorks } from '@/types/worksType';
 
 export const metadata = {
-  title: "Works",
+  title: 'Works',
 };
 
 const WorksPage = () => {
   let filteredWorks: IWorks[] = [];
 
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'production') {
     filteredWorks = works.filter((work) => work.published);
   }
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'development') {
     filteredWorks = works;
   }
 
   return (
-    <main className="flex-1">
-      <WorksBanner image={worksBannerImage} title="WORKS" />
-      <section className="relative z-[1] mx-auto max-w-screen-lg  pb-20 pt-8 lg:max-w-screen-2xl lg:px-6">
-        <PageTitle title="WORKS" />
-        <ul className="flex flex-wrap gap-4 px-5 sm:grid sm:grid-cols-2 sm:grid-rows-[repeat(auto-fill,minmax(0,1fr))] lg:grid-cols-3 lg:px-0 xl:grid-cols-4">
+    <main className='flex-1'>
+      <WorksBanner image={worksBannerImage} title='WORKS' />
+      <section className='relative z-[1] mx-auto max-w-screen-lg pt-8 pb-20 lg:max-w-screen-2xl lg:px-6'>
+        <PageTitle title='WORKS' />
+        <ul className='flex flex-wrap gap-4 px-5 sm:grid sm:grid-cols-2 sm:grid-rows-[repeat(auto-fill,minmax(0,1fr))] lg:grid-cols-3 lg:px-0'>
           {filteredWorks.map((work) => (
             <WorkItem key={work.id} work={work} />
           ))}
